69ec075a73d509262f39594d0bc69deb4cc0873d
James Thomas <jamesjacobthomas7@gmail.com>
2018-04-30 23:55:37 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -11,7 +11,6 @@ def loop_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
    counter = 0
    loop do 
-     
      puts phrase
      counter += 1
      if counter < number_of_times
